dee3c957d70d6d3be4239a2c6f5246b46854d067
jeremyrs <jeremyrschneider@gmail.com>
2015-03-29 19:01:48 -0700

    upadte skip with fruit that start with P and edit my list is with + feature

--- students/JeremySchneider/session03/list_lab.py
+++ students/JeremySchneider/session03/list_lab.py
@@ -13,6 +13,23 @@ food.append(m)
 print food
 
 number = raw_input(u'Pick a number.')
+
+#int is required otherwise food[] causes error
+#error message expecting integer
+
 n = int(number)
 
 print food[n]
+
+new_fruit = raw_input(u'choose an other fruit')
+
+j = new_fruit
+
+food.insert(0, j)
+print food
+"""Iknow assignment wanted me to do this with the
+'+' when i tried i kept getting errors so I only used
+.insert()"""
+
+
+
